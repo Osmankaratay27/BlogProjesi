22732de8f1b4f19a897a5b0e85bef3345cf9de4f
Osmankaratay27 <osmankaratay754@gmail.com>
2025-01-22 17:26:38 +0300

    Yazarın diğer yazıları kısmı view cmponent ile yapıldı ve proje .NET9 sürümüne yükseltildi

---
+++ BlogProjesi/ViewComponents/Blog/WriterLastBlog.cs
@@ -0,0 +1,18 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogProjesi.ViewComponents.Blog
+{
+    public class WriterLastBlog : ViewComponent
+    {
+        BlogManager bm = new BlogManager(new EfBlogRepository());
+
+        public IViewComponentResult Invoke()
+        {
+
+            var values = bm.GetBlogListByWriter(1);
+            return View(values);
+        }
+    }
+}
