e4419413c4707aa3f66d18d2fb1ffa7c1a8afa68
Osmankaratay27 <osmankaratay754@gmail.com>
2025-01-23 18:45:10 +0300

    Authorization , Authentication ve Login işlemi yapıldı.

--- BlogProjesi/Controllers/LoginController.cs
+++ BlogProjesi/Controllers/LoginController.cs
@@ -1,12 +1,47 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DataAccessLayer.Concrete;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.BlazorIdentity.Pages;
+using Microsoft.Win32;
+using System.Security.Claims;
 
 namespace BlogProjesi.Controllers
 {
+    [AllowAnonymous]
     public class LoginController : Controller
     {
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
         }
+        [HttpPost]
+        public async Task<IActionResult> Index(Writer p)
+        {
+            Context c = new Context();
+            var datavalue = c.Writers.FirstOrDefault(x => x.WriterMail == p.WriterMail && x.WriterPassword == p.WriterPassword);
+            if (datavalue != null)
+            {
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name,p.WriterMail)
+                };
+
+                var useridentity = new ClaimsIdentity(claims, "a");
+                ClaimsPrincipal principal = new ClaimsPrincipal(useridentity);
+                await HttpContext.SignInAsync(principal);
+             
+                    return RedirectToAction("Index", "Writer");
+            }
+            else
+            {
+                return View();
+            }
+        }
     }
+
+
+    
 }
--- BlogProjesi/Program.cs
+++ BlogProjesi/Program.cs
@@ -1,7 +1,36 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
+builder.Services.AddMvc(config =>
+{
+    var policy = new AuthorizationPolicyBuilder()
+                     .RequireAuthenticatedUser()
+                     .Build();
+    config.Filters.Add(new AuthorizeFilter(policy));
+});
+builder.Services.AddMvc();
+
+builder.Services.AddAuthentication(
+    CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(x =>
+    {
+        x.LoginPath = "/Login/Index";
+    });
+
+builder.Services.ConfigureApplicationCookie(optipns =>
+{
+    //Cookie settings
+    optipns.Cookie.HttpOnly = true;
+    optipns.ExpireTimeSpan = TimeSpan.FromMinutes(5);
+
+    optipns.LoginPath = "/Login/Index";
+    optipns.SlidingExpiration = true;
+});
 
 var app = builder.Build();
 
@@ -17,6 +46,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseAuthentication();
 
 app.UseAuthorization();
 
