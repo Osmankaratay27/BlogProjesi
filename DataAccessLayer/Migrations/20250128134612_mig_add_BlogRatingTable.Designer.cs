5ccf7bad41def44b2d2b2f345633ff79bfd6d6dc
Osmankaratay27 <osmankaratay754@gmail.com>
2025-01-28 17:18:09 +0300

    Blogların yorumlar ile puanlandırılma için BlogRatings tablosu oluşturuldu. Trigger ile update edilmesi sağlandı

--- DataAccessLayer/Migrations/20250128134612_mig_add_BlogRatingTable.Designer.cs
+++ DataAccessLayer/Migrations/20250128134612_mig_add_BlogRatingTable.Designer.cs
@@ -4,6 +4,7 @@ using DataAccessLayer.Concrete;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DataAccessLayer.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20250128134612_mig_add_BlogRatingTable")]
+    partial class mig_add_BlogRatingTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -85,6 +88,9 @@ namespace DataAccessLayer.Migrations
                         .HasColumnType("nvarchar(max)");
 
                     b.Property<int>("CategoryID")
+                        .HasColumnType("int");
+
+                    b.Property<int>("CommentID")
                         .HasColumnType("int");
 
                     b.Property<int>("WriterID")
---
+++ DataAccessLayer/Migrations/20250128140139_mig_add_Blog_Table_Fixed_Delete_CommentIDColumn.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig_add_Blog_Table_Fixed_Delete_CommentIDColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CommentID",
+                table: "Blogs");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CommentID",
+                table: "Blogs",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
