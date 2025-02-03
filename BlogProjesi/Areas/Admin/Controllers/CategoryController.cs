aa818e5c1d25ad0ead79f716f15690c8d403807c
Osmankaratay27 <osmankaratay754@gmail.com>
2025-02-03 17:50:49 +0300

    Admin panelinde kategori ekleme sayfası yapıldı

--- BlogProjesi/Areas/Admin/Controllers/CategoryController.cs
+++ BlogProjesi/Areas/Admin/Controllers/CategoryController.cs
@@ -1,5 +1,8 @@
 ﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Mvc;
 using X.PagedList;
 using X.PagedList.Extensions;
@@ -15,6 +18,34 @@ namespace BlogProjesi.Areas.Admin.Controllers
         {
             var values = cm.GetList().ToPagedList(page,3);
             return View(values);
+        }
+        [HttpGet]
+        public IActionResult AddCategory()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddCategory(Category p)
+        {
+       
+            CategoryValidator cv = new CategoryValidator();
+            ValidationResult results = cv.Validate(p);
+
+            if (results.IsValid)
+            {
+                p.CategoryStatus = true;
+                cm.TAdd(p);
+                return RedirectToAction("Index", "Category");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+
+            return View();
         }
     }
 }
