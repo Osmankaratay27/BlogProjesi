02c9782f6b24166c6e0f58a461c057c1220e0149
Osmankaratay27 <osmankaratay754@gmail.com>
2025-02-03 15:24:18 +0300

    AdminLayout'daki navbar partiala ayrıldı ve düzenlendi

--- BlogProjesi/Controllers/AdminController.cs
+++ BlogProjesi/Controllers/AdminController.cs
@@ -8,5 +8,9 @@ namespace BlogProjesi.Controllers
         {
             return View();
         }
+        public PartialViewResult AdminNavbarPartial()
+        {
+            return PartialView();
+        }
     }
 }
