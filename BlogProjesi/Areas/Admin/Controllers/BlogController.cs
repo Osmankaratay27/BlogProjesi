72d371744933fc9ea3d8afa44b497c06b6a3a344
Osmankaratay27 <osmankaratay754@gmail.com>
2025-02-04 16:02:04 +0300

    Static excel export işlemi yapıldı

---
+++ BlogProjesi/Areas/Admin/Controllers/BlogController.cs
@@ -0,0 +1,54 @@
+﻿using BlogProjesi.Areas.Admin.Models;
+using ClosedXML.Excel;
+using DocumentFormat.OpenXml.Office2013.Drawing.ChartStyle;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogProjesi.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class BlogController : Controller
+    {
+        public IActionResult ExportStaticExcelBlogList()
+        {
+            using (var workbook = new XLWorkbook())
+            {
+                var worksheet = workbook.Worksheets.Add("Blog Listesi");
+                worksheet.Cell(1, 1).Value = "Blog ID";
+                worksheet.Cell(1, 2).Value = "Blog Adı";
+
+                int BlogRowCount = 2;
+                foreach (var item in GetBlogList())
+                {
+                    worksheet.Cell(BlogRowCount, 1).Value = item.ID;
+                    worksheet.Cell(BlogRowCount, 2).Value = item.BlogName;
+                    BlogRowCount++;
+                }
+
+                using (var stream = new MemoryStream())
+                {
+                    workbook.SaveAs(stream);
+                    var content=stream.ToArray();
+                    return File(content,"application/vnd.openxmlformats-officedocument.spreadsheet.ml.sheet","Calisma1.xlsx");
+                }
+            }
+
+        }
+
+        public List<BlogModel> GetBlogList()
+        {
+            List<BlogModel> bm = new List<BlogModel>
+            {
+                new BlogModel{ID=1,BlogName="C# ile Programlamaya Giriş"},
+                new BlogModel{ID=2,BlogName="Tesla Firmasının Araçları"},
+                new BlogModel{ID=3,BlogName="2025 Olimpiyatları"}
+            };
+            return bm;
+
+        }
+
+        public IActionResult BlogListExcel()
+        {
+            return View();
+        }
+    }
+}
---
+++ BlogProjesi/Areas/Admin/Models/BlogModel.cs
@@ -0,0 +1,8 @@
+﻿namespace BlogProjesi.Areas.Admin.Models
+{
+    public class BlogModel
+    {
+       public int ID { get; set; }
+       public string BlogName { get; set; }
+    }
+}
