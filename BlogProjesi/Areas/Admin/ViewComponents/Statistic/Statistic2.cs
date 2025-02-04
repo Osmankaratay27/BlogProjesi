651de63971700e983ff65af6b78a6690a0461f92
Osmankaratay27 <osmankaratay754@gmail.com>
2025-02-04 17:50:19 +0300

    İstatistik Düzenlemeleri 2

---
+++ BlogProjesi/Areas/Admin/ViewComponents/Statistic/Statistic2.cs
@@ -0,0 +1,18 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogProjesi.Areas.Admin.ViewComponents.Statistic
+{
+    public class Statistic2 : ViewComponent
+    {
+    
+        Context c = new Context();
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.LastBlog = c.Blogs.OrderByDescending(x => x.BlogID).Select(x => x.BlogTitle).Take(1).FirstOrDefault();
+            return View();
+        }
+    }
+}
