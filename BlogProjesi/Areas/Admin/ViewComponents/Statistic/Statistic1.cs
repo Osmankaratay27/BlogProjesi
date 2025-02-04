b503162d7a49d67334387ccbec49aec5ca5d6e93
Osmankaratay27 <osmankaratay754@gmail.com>
2025-02-04 17:30:25 +0300

    Widget Sayfasındaki  İstatistikler için ViewComponent eklendi

---
+++ BlogProjesi/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogProjesi.Areas.Admin.ViewComponents.Statistic
+{
+    public class Statistic1:ViewComponent
+    {
+        BlogManager bm= new BlogManager(new EfBlogRepository());
+        Context c = new Context();
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.BlogCount=bm.GetList().Count();
+            ViewBag.MessageCount=c.Contacts.Count();
+            ViewBag.CommentCount=c.Comments.Count();
+            return View();
+        }
+    }
+}
