989e77a1276773ddff056fca1ed4f73753bce35c
Osmankaratay27 <osmankaratay754@gmail.com>
2025-02-05 14:27:24 +0300

    İstatistik Düzenlemeleri 3

---
+++ BlogProjesi/Areas/Admin/ViewComponents/Statistic/Statistic4.cs
@@ -0,0 +1,17 @@
+﻿using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogProjesi.Areas.Admin.ViewComponents.Statistic
+{
+    public class Statistic4 : ViewComponent
+    {
+        Context c = new Context();
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.AdminName =c.Admins.Where(x=>x.AdminID==2).Select(x=>x.Name).FirstOrDefault();
+            ViewBag.AdminImage =c.Admins.Where(x=>x.AdminID==2).Select(x=>x.ImageUrl).FirstOrDefault();
+            ViewBag.AdminShortDescription =c.Admins.Where(x=>x.AdminID==2).Select(x=>x.ShortDescription).FirstOrDefault();
+            return View();
+        }
+    }
+}
