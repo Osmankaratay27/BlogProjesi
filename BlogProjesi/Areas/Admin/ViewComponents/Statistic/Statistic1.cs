2cace773fa1d73ced4e2ca67cbd532e2ee6faf57
Osmankaratay27 <osmankaratay754@gmail.com>
2025-02-05 14:59:23 +0300

    Api üzerinden hava sıcaklığı çekildi

--- BlogProjesi/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
+++ BlogProjesi/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
@@ -2,6 +2,7 @@
 using DataAccessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
 using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
 
 namespace BlogProjesi.Areas.Admin.ViewComponents.Statistic
 {
@@ -14,6 +15,13 @@ namespace BlogProjesi.Areas.Admin.ViewComponents.Statistic
             ViewBag.BlogCount=bm.GetList().Count();
             ViewBag.MessageCount=c.Contacts.Count();
             ViewBag.CommentCount=c.Comments.Count();
+
+            string api = "fefedcd91fcba23ee66046a05c4d49f3";
+            string city = "istanbul";
+            string connection= "https://api.openweathermap.org/data/2.5/weather?q="+city+"&mode=xml&lang=tr&units=metric&appid=" + api;
+            XDocument document= XDocument.Load(connection);
+            ViewBag.WheatherTemperature = document.Descendants("temperature").ElementAt(0).Attribute("value").Value;
+
             return View();
         }
     }
