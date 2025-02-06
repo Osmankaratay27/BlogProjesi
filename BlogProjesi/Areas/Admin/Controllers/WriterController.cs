058981d3fa9f397acb7d636310288ca96043337d
Osmankaratay27 <osmankaratay754@gmail.com>
2025-02-06 15:33:25 +0300

    Ajax ile silme işlemi yapıldı

--- BlogProjesi/Areas/Admin/Controllers/WriterController.cs
+++ BlogProjesi/Areas/Admin/Controllers/WriterController.cs
@@ -23,6 +23,13 @@ namespace BlogProjesi.Areas.Admin.Controllers
             var jsonWriters = JsonConvert.SerializeObject(w);
             return Json(jsonWriters);
         }
+        [HttpPost]
+        public IActionResult DeleteWriter(int id)
+        {
+          var writer=writers.FirstOrDefault(x => x.Id == id);
+           writers.Remove(writer);
+            return Json(writer);
+        }
         public IActionResult GetWriterByID(int writerid)
         {
             var findWriter=writers.FirstOrDefault(x => x.Id == writerid);
