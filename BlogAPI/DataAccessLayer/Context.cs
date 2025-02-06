2c89a238a8fff2f6c907f36e8a254fd47f12d194
Osmankaratay27 <osmankaratay754@gmail.com>
2025-02-06 17:07:30 +0300

    API katmanı eklendi

---
+++ BlogAPI/DataAccessLayer/Context.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlogAPI.DataAccessLayer
+{
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-56OJ44O; database=CoreBlogApiDb; integrated security=true;TrustServerCertificate=true;");
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ BlogAPI/DataAccessLayer/Employee.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+
+namespace BlogAPI.DataAccessLayer
+{
+    public class Employee
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+}
