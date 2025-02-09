3dea5d2767f81a91e8e0516dd4ff12f23c1c2c59
Osmankaratay27 <osmankaratay754@gmail.com>
2025-02-09 17:04:50 +0300

    Api ile veri getirme işlemi yapıldı

---
+++ BlogAPI/Controllers/DefaultController.cs
@@ -0,0 +1,19 @@
+﻿using BlogAPI.DataAccessLayer;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult EmployeeList()
+        {
+            using var c = new Context();
+            var values = c.Employees.ToList();
+            return Ok(values);
+        }
+    }
+}
