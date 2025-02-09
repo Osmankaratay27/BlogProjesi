c4f1b78c8359bc1be2c9886a8ef03faf40b8628e
Osmankaratay27 <osmankaratay754@gmail.com>
2025-02-09 18:46:28 +0300

    Api ile projenin haberleşmesi sağlandı

---
+++ BlogProjesi/Controllers/EmployeeTestController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace BlogProjesi.Controllers
+{
+    public class EmployeeTestController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var httpClient = new HttpClient();
+            var responseMessage = await httpClient.GetAsync("https://localhost:44328/api/Default/EmployeeList");
+            var jsonString =  await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<Class1>>(jsonString);
+            return View(values);
+        }
+
+        public class Class1
+        {
+            public int ID { get; set; }
+            public string Name { get; set; }
+        }
+    }
+}
