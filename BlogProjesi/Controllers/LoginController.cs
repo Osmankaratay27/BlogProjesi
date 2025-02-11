649f538cec8529e22fa67f886b76b5a0bacdb7c4
Osmankaratay27 <osmankaratay754@gmail.com>
2025-02-11 19:24:00 +0300

    Identity ile Login işlemi yapıldı

--- BlogProjesi/Controllers/LoginController.cs
+++ BlogProjesi/Controllers/LoginController.cs
@@ -1,7 +1,9 @@
-﻿using DataAccessLayer.Concrete;
+﻿using BlogProjesi.Models;
+using DataAccessLayer.Concrete;
 using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.BlazorIdentity.Pages;
 using Microsoft.Win32;
@@ -12,34 +14,56 @@ namespace BlogProjesi.Controllers
     [AllowAnonymous]
     public class LoginController : Controller
     {
+       private readonly SignInManager<AppUser> _signInManager;
+
+        public LoginController(SignInManager<AppUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
         [HttpGet]
         public IActionResult Index()
         {
             return View();
         }
         [HttpPost]
-        public async Task<IActionResult> Index(Writer p)
+        public async Task<IActionResult> Index(UserSignInViewModel p)
         {
-            Context c = new Context();
-            var datavalue = c.Writers.FirstOrDefault(x => x.WriterMail == p.WriterMail && x.WriterPassword == p.WriterPassword);
-            if (datavalue != null)
+            if (ModelState.IsValid)
             {
-                var claims = new List<Claim>
+                var result = await _signInManager.PasswordSignInAsync(p.username, p.password, false, true);
+                if (result.Succeeded)
                 {
-                    new Claim(ClaimTypes.Name,p.WriterMail)
-                };
-
-                var useridentity = new ClaimsIdentity(claims, "a");
-                ClaimsPrincipal principal = new ClaimsPrincipal(useridentity);
-                await HttpContext.SignInAsync(principal);
-             
                     return RedirectToAction("Index", "Dashboard");
+                }
             }
-            else
-            {
-                return View();
-            }
+          
+           return View();
         }
+        //Identity olmadan önceki Claim ile yazılışı
+        //[HttpPost]
+        //public async Task<IActionResult> Index(Writer p)
+        //{
+        //    Context c = new Context();
+        //    var datavalue = c.Writers.FirstOrDefault(x => x.WriterMail == p.WriterMail && x.WriterPassword == p.WriterPassword);
+        //    if (datavalue != null)
+        //    {
+        //        var claims = new List<Claim>
+        //        {
+        //            new Claim(ClaimTypes.Name,p.WriterMail)
+        //        };
+
+        //        var useridentity = new ClaimsIdentity(claims, "a");
+        //        ClaimsPrincipal principal = new ClaimsPrincipal(useridentity);
+        //        await HttpContext.SignInAsync(principal);
+
+        //        return RedirectToAction("Index", "Dashboard");
+        //    }
+        //    else
+        //    {
+        //        return View();
+        //    }
+        //}
     }
 
 
