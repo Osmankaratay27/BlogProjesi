fed38749c2d13fd45412409b941cc381320f057c
Osmankaratay27 <osmankaratay754@gmail.com>
2025-02-12 16:06:16 +0300

    Identity ile giriş yapan yazarın bilgileri getirildi

--- BlogProjesi/ViewComponents/Writer/WriterAboutOnDashboard.cs
+++ BlogProjesi/ViewComponents/Writer/WriterAboutOnDashboard.cs
@@ -1,4 +1,6 @@
-﻿using BusinessLayer.Concrete;
+﻿
+using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,10 +9,13 @@ namespace BlogProjesi.ViewComponents.Writer
     public class WriterAboutOnDashboard:ViewComponent
     {
         WriterManager wm=new WriterManager(new EfWriterRepository());
+        Context c = new Context();
         public IViewComponentResult Invoke()
         {
-
-            var values = wm.GetByFilter(User.Identity.Name);
+    
+            var userName = User.Identity.Name;
+            var userMail= c.Users.Where(x=>x.UserName == userName).Select(x=>x.Email).FirstOrDefault();
+            var values = wm.GetByFilter(userMail);
             return View(values);
         }
     }
