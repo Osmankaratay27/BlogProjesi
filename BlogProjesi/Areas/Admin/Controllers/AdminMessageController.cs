cdd793c58c4b7048b2821deab899331861c8485b
Osmankaratay27 <osmankaratay754@gmail.com>
2025-02-18 21:29:11 +0300

    Admin panelinde mesaj kutusu yapıldı

---
+++ BlogProjesi/Areas/Admin/Controllers/AdminMessageController.cs
@@ -0,0 +1,30 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace BlogProjesi.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AdminMessageController : Controller
+    {
+        MessageManager mm = new MessageManager(new EfMessageRepository());
+        public IActionResult Inbox()
+        {
+            int id = Convert.ToInt32(User.FindFirstValue(ClaimTypes.NameIdentifier));
+            var values = mm.GetInboxListByWriter(id);
+            return View(values);
+        }
+        public IActionResult Sendbox()
+        {
+            int id = Convert.ToInt32(User.FindFirstValue(ClaimTypes.NameIdentifier));
+            var values = mm.GetSendboxListByWriter(id);
+            return View(values);
+
+        }
+        public IActionResult ComposeMessage()
+        {
+            return View();  
+        }
+    }
+}
