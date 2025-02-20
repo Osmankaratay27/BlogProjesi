b1fc83864362319cf2bf6e4066c2e231905bd178
Osmankaratay27 <osmankaratay754@gmail.com>
2025-02-20 15:13:43 +0300

    Admin panelinde Blog listesi sayfası yapıldı

---
+++ BlogProjesi/Areas/Admin/Controllers/AdminBlogController.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogProjesi.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AdminBlogController : Controller
+    {
+        BlogManager bm = new BlogManager(new EfBlogRepository());
+        public IActionResult Index()
+        {
+            var values = bm.GetBlogListWithCategory();
+            return View(values);
+        }
+    }
+}
