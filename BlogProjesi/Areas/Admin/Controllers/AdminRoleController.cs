8a1cf47966be430e52fbc73a2bf59f9663ad0ebe
Osmankaratay27 <osmankaratay754@gmail.com>
2025-02-20 17:55:53 +0300

    Admin panelinde Rol ekleme sayfası yapıldı

---
+++ BlogProjesi/Areas/Admin/Controllers/AdminRoleController.cs
@@ -0,0 +1,54 @@
+﻿using BlogProjesi.Models;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogProjesi.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AdminRoleController : Controller
+    {
+        private readonly RoleManager<AppRole> _roleManager;
+
+        public AdminRoleController(RoleManager<AppRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+        public IActionResult Index()
+        {
+            var values = _roleManager.Roles.ToList();
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult AddRole()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async  Task<IActionResult> AddRole(RoleViewModel p)
+        {
+            if (ModelState.IsValid)
+            {
+                AppRole role=new AppRole
+                {
+                    Name=p.name
+                };
+
+                var result =await _roleManager.CreateAsync(role);
+                if(result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    foreach (var item in result.Errors)
+                    {
+                        ModelState.AddModelError("", item.Description);
+                    }
+                }
+            }
+            return View();
+        }
+    }
+}
